07ec2ad1710fa84513ad5c46e06fd94492ea09c8
Monika <moni.kesarwani@yahoo.com>
2019-09-26 23:05:32 -0400

    changed in networking method

--- BitcoinTicker/ViewController.swift
+++ BitcoinTicker/ViewController.swift
@@ -7,15 +7,38 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+
+/*The UIPicker is the scroll wheel that shows all the available currencies that we can query for a Bitcoin price. for that we have to conform UIPickerViewDataSource, UIPickerViewDelegate in our viewcontroller as well as its delegates methods too.
+ First, add the method numberOfComponents(in:) to determine how many columns we want in our picker.
+ 1.numberOfComponents
+ Next, we need to tell Xcode how many rows this picker should have using the pickerView(numberOfRowsInComponent:) method.
+ 2.numberOfRowsInComponent
+ Finally, let’s fill the picker row titles with the Strings from our currencyArray using the pickerView:titleForRow: method.
+ 3.titleForRow
+
+ Place this delegate method below the other methods we just created to tell the picker what to do when the user selects a particular row.
+ 4.didSelectRow
+
+ and also mention that this class will the delegate of this protoclo therefore we wrote these two line of code in viewDidLoad
+ currencyPicker.delegate = self
+ currencyPicker.dataSource = self
+ 
+ */
 
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
-    
-    
-    
-    
+    var ask: Double = 0.0
     let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    
     var currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    let currencySymbolsArray = ["$", "R$", "$", "¥", "€", "£", "$", "Rp", "₪", "₹", "¥", "$", "kr", "$", "zł", "lei", "₽", "kr", "$", "$", "R"]
+    
+    var currentSymbol = ""
+    
+    //to make finalURL add baseURL + currencyArray[row]
     var finalURL = ""
 
     //Pre-setup IBOutlets
@@ -27,6 +50,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         currencyPicker.delegate = self
         currencyPicker.dataSource = self
        
@@ -35,69 +59,88 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     //TODO: Place your 3 UIPickerView delegate methods here
     
+    // determine number of columns
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
+    
+    // determine number of rows
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return currencyArray.count
     }
     
+    // fill in each row with title from array
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return currencyArray[row]
     }
 
+    /*We have all the currency names stored in the currencyArray already. Let’s use that and the row selected to make up the finalURL.
+     https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC<Currency>
+
+     e.g. https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD
+     
+     baseURL made is stored in a variable. so we also to add baseURL + currencyArray[row] to make finalURL */
+    
+    
+    // Print something when you select a row
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        print(currencyArray[row])
+        //print(currencyArray[row])
         
         finalURL = baseURL + currencyArray[row]
-        print(finalURL)
+        //print(finalURL)
+        
+        currentSymbol = currencySymbolsArray[row]
+      
+        getCurencyData(url: finalURL)
+        
+        
         
     }
     
-    
-//    
 //    //MARK: - Networking
 //    /***************************************************************/
-//    
-//    func getWeatherData(url: String, parameters: [String : String]) {
-//        
-//        Alamofire.request(url, method: .get, parameters: parameters)
-//            .responseJSON { response in
-//                if response.result.isSuccess {
-//
-//                    print("Sucess! Got the weather data")
-//                    let weatherJSON : JSON = JSON(response.result.value!)
-//
-//                    self.updateWeatherData(json: weatherJSON)
-//
-//                } else {
-//                    print("Error: \(String(describing: response.result.error))")
-//                    self.bitcoinPriceLabel.text = "Connection Issues"
-//                }
-//            }
-//
-//    }
-//
-//    
-//    
-//    
-//    
+   
+    func getCurencyData(url: String) {
+        
+        Alamofire.request(url, method: .get)
+            .responseJSON { response in
+                if response.result.isSuccess {
+                    print("Sucess! Got Currency data")
+                    
+                    let currencyJSON : JSON = JSON(response.result.value!)
+                    print(currencyJSON)
+
+                    self.updateData(json: currencyJSON)
+
+                } else {
+                    print("Error: \(String(describing: response.result.error))")
+                    
+                    self.bitcoinPriceLabel.text = "Connection Issues"
+                }
+            }
+
+    }
+
+    
+    
+    
+    
 //    //MARK: - JSON Parsing
 //    /***************************************************************/
-//    
-//    func updateWeatherData(json : JSON) {
-//        
-//        if let tempResult = json["main"]["temp"].double {
-//        
-//        weatherData.temperature = Int(round(tempResult!) - 273.15)
-//        weatherData.city = json["name"].stringValue
-//        weatherData.condition = json["weather"][0]["id"].intValue
-//        weatherData.weatherIconName =    weatherData.updateWeatherIcon(condition: weatherData.condition)
-//        }
-//        
-//        updateUIWithWeatherData()
-//    }
-//    
+    
+    func updateData(json : JSON) {
+        
+        if let currencyResult = json["ask"].double {
+            bitcoinPriceLabel.text = "\(currentSymbol) \(currencyResult)"
+
+        } else{
+            bitcoinPriceLabel.text = "Currency Unavailable"
+        }
+        
+        
+       // updateUIWithWeatherData()
+    }
+    
 
 
 
